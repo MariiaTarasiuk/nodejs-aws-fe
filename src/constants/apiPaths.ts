e79caeae66377756615ffbc3f32bef07d8f86cff
MariiaTarasiuk <72539820+MariiaTarasiuk@users.noreply.github.com>
2020-12-09 19:06:28 +0200

    feat: task 8 (#6)
    * get token from localStorage
    
    * Add cart api integration
    
    * add alerts
    
    * add cart api path
    
    * fix for http requests
    
    * lost api
    
    Co-authored-by: Mariia Tarasiuk <mariia.tarasiuk.1@gmail.com>
    Co-authored-by: Herman_Bilous <Herman_Bilous@epam.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,6 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://g8n64lq9sd.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://0xyi26x4qh.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://develop.eba-jmgr9esn.eu-west-1.elasticbeanstalk.com/api"
 };
 
 export default API_PATHS;
